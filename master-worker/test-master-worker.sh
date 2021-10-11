c251e804f8e9d076051a1c575c5f5c6b9f18aadc
devansh-dvj <devansh.dvj@gmail.com>
2021-10-11 19:52:11 +0530

    A: Updated test script - works

--- master-worker/test-master-worker.sh
+++ master-worker/test-master-worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script takes 4 arguments that are given to the master worker program
 
-gcc -o master-worker master-worker.c -lpthread
-./master-worker $1 $2 $3 $4 >output
+gcc -o master-worker.o master-worker.c -lpthread
+./master-worker.o $1 $2 $3 $4 >output
 awk -f check.awk MAX=$1 output
