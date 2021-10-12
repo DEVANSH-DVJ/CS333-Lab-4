b0cd2b84ef866fd83c88cccaddf76d8247fd3948
devansh-dvj <devansh.dvj@gmail.com>
2021-10-13 01:14:24 +0530

    C: Updated test script - zem works

--- zemaphore/test-zem.sh
+++ zemaphore/test-zem.sh
@@ -1,7 +1,7 @@
 echo "Running test-zem.c"
-g++ test-zem.c zemaphore.c -o test-zem -lpthread
-./test-zem
+g++ test-zem.c zemaphore.c -o test-zem.o -lpthread
+./test-zem.o
 echo
 echo "Running test-toggle.c"
-g++ test-toggle.c zemaphore.c -o test-toggle -lpthread
-./test-toggle
+g++ test-toggle.c zemaphore.c -o test-toggle.o -lpthread
+./test-toggle.o
