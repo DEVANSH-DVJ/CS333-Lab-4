53a6d2f078f6d9c9093b4b790320ecb0fdfe5900
devansh-dvj <devansh.dvj@gmail.com>
2021-10-13 01:00:38 +0530

    C: Started zemaphore

---
+++ zemaphore/zemaphore.c
@@ -0,0 +1,15 @@
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <wait.h>
+
+#include "zemaphore.h"
+
+void zem_init(zem_t *s, int value) {}
+
+void zem_down(zem_t *s) {}
+
+void zem_up(zem_t *s) {}
